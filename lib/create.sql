822481367fe7c63fecb0b203dbcd3d01a6ee4462
Buddha Lo <lo_977@yahoo.com>
2025-01-07 00:11:48 -0800

    done

---
+++ lib/create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE bears(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN
+);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Mr.Chocolate", 4,"M","Dark Brown","Not so Friendly", True);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Rowdy", 2,"F"," Brown","Not so Aggressive ", True);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Tabitha", 4,"F","Gray","Friendly", True);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown", 8,"M","Light Brown","Friendly", True);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Melissa", 7,"F","  Dark Gray","Not so Friendly", False);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Grinch", 5,"M","Black ","Friendly", True);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Wendy", 4,"F","Dark Black","so Friendly", False);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES (NULL, 4,"M","Dark Brown","Not so Friendly", True);
