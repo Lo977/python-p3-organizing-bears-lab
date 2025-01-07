82ec82edb59c2c3acf4a07020a48995e95c2b8a1
Buddha Lo <lo_977@yahoo.com>
2025-01-07 00:24:52 -0800

    done

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,15 @@
-INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Mr.Chocolate", 4,"M","Dark Brown","Not so Friendly", True);
-INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Rowdy", 2,"F"," Brown","Not so Aggressive ", True);
-INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Tabitha", 4,"F","Gray","Friendly", True);
-INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown", 8,"M","Light Brown","Friendly", True);
-INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Melissa", 7,"F","  Dark Gray","Not so Friendly", False);
-INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Grinch", 5,"M","Black ","Friendly", True);
-INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Wendy", 4,"F","Dark Black","so Friendly", False);
-INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES (NULL, 4,"M","Dark Brown","Not so Friendly", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 4, 'M', 'Choco Brown', 'mean', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Rowdy', 9, 'M', 'brown', 'frildy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Tabitha', 8, 'F', ' dark gray', 'mean', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 4, 'M', 'light brown', 'aggressive',False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Melissa', 2, 'F', 'brown black', 'loving', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Grinch', 7, 'M', 'super brown', 'mean', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Wendy', 4, 'F', 'med gray', 'quite', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 4, 'F', 'dark black', 'active', True);
